11430a9b7b205ab904916855ace95bf7397c4563
Alleff Dymytry <alleffdymytry@gmail.com>
2024-08-21 08:53:59 -0300

    First Commit

---
+++ MainPage.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit_antd_components as sac
+
+class MultiPageApp:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    mpa = MultiPageApp()
